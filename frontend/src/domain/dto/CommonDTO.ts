70d41aee2f32a827c2ab927a18d4f3924a43be63
Lucas-Song-WS <lucas.song.ws@gmail.com>
2025-08-17 13:08:42 +0800

    add pagination DTOs

--- frontend/src/domain/dto/CommonDTO.ts
+++ frontend/src/domain/dto/CommonDTO.ts
@@ -2,3 +2,16 @@ export interface SelectionDTO {
   seq: number;
   name: string;
 }
+
+export interface PaginationDTO {
+  pageNumber: number;
+  pageSize: number;
+  sortField?: string;
+  sortDirection?: "asc" | "desc";
+}
+
+export interface PagesDTO {
+  totalRecords: number;
+  pageSize: number;
+  totalPages: number;
+};
